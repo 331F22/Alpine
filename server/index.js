75b4bb8de548fdc1f92c820625ce24d41aa48968
Jzavarelli <jlzavarelli@gmail.com>
2022-11-28 14:51:46 -0700

    jace-update-0.1

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 // READ
 app.get("/api/read", (req, res) => {
     const sqlSelect = "SELECT * FROM volunteers;"
-    db.query(sqlSelect, (err, result) => {
+    db.query(sqlSelect, (err, result) => { 
         if(err){
             throw err;
         }
@@ -28,18 +28,20 @@ app.get("/api/read", (req, res) => {
     })
 })
 
-// CREATE
+// CREATE - "INSERT INTO volunteers (first_name, last_name, email_address, white_list, reason) VALUES (?,?,?,?,?);"
 app.post("/api/create", (req, res) => {
     const fn = req.body.first
     const ln = req.body.last
     const ea = req.body.email
+    // const wht = req.body.whitelist => AutoSet
+    // const reas = req.body.reason => AutoSet
     const sqlInsert = "INSERT INTO volunteers (first_name, last_name, email_address) VALUES (?,?,?);"
-    db.query(sqlInsert, [fn, ln, ea], (err, result) => {
+    db.query(sqlInsert, [fn, ln, ea/*, wht, reas*/], (err, result) => {
         if(err) throw err
         console.log("Server posted: ", fn, ln)
         res.send(result)
     })
-})
+}) 
 
 // DELETE
 app.delete("/api/delete/:emailAddress", (req, res) => {
@@ -50,13 +52,13 @@ app.delete("/api/delete/:emailAddress", (req, res) => {
         if(err) throw err
         console.log("Server: deleted: ", ea)
         res.send(result)
-    })
+    }) 
 })
 
 // UPDATE
 app.put("/api/update", (req, res) => {
     // console.log(req)
-
+    
     const ne = req.body.new;
     const oe = req.body.old;
     console.log("Ready to change: ", oe, "to", ne)
@@ -68,7 +70,26 @@ app.put("/api/update", (req, res) => {
     })
 })
 
-const PORT = process.env.EXPRESSPORT;
+// UPDATE - BLACKLIST
+app.put("/api/blacklist", (req, res) => {
+    // console.log(req)
+    
+    // const ne = req.body.new;
+    // const oe = req.body.old;
+    const wht = req.body.whitelist;
+    const reas = req.body.reason;
+    const oe = req.body.old;
+
+    // console.log("Ready to change: ", oe, "to", ne)
+    const sqlUpdate = "UPDATE volunteers SET white_list = ?, reason = ? WHERE email_address = ?"
+    db.query(sqlUpdate, [wht, reas, oe], (err, result)=>{
+        if(err)  throw err;
+        console.log("Server changed: ", wht, reas, "to", ne)
+        res.send(result)
+    })
+})
+
+const PORT = 3046 //process.env.EXPRESSPORT;
 const msg = `Running on PORT ${PORT}`
 app.get("/", (req, res) => {
     res.send(`<h1>Express Server</h1><p>${msg}<p>`)
