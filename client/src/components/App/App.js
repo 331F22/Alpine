8ec6792670866ef298bbb80e88c594ae5d9b44e4
ali <ali.khaef2013@gmail.com>
2022-11-28 18:31:12 -0700

    app-update-frontend-ali

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,23 +1,68 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 import AddEntry from '../AddEntry.jsx';
 import CurrentEntries from '../CurrentEntries.jsx';
-import Footer from '../Footer.jsx'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Entries</h1>
+      <h1>Admin Page</h1>
 
-      <AddEntry />
+      {/* <AddEntry /> */}
+      {/* <hr /> */}
+      {/* <CurrentEntries /> */}
+      {/* <hr /> */}
+      <button>Click Me Boi!</button>
       <hr />
-      <CurrentEntries />
+
+      <table class="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th scope="col">First Name</th>
+            <th scope="col">Last Name</th>
+            <th scope="col">Listing</th>
+            <th scope="col">Reason</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td scope="row">first_name</td>
+            <td>last_name</td>
+            <td>white_listed</td>
+            <td>reasoning is followed</td>
+          </tr>
+          <tr>
+            <td scope="row">first_name</td>
+            <td>last_name</td>
+            <td>white_listed</td>
+            <td>reasoning is followed</td>
+          </tr>
+          <tr>
+            <td scope="row">first_name</td>
+            <td>last_name</td>
+            <td>white_listed</td>
+            <td>reasoning is followed</td>
+          </tr>
+        </tbody>
+      </table>
+
       <hr />
-      <Footer />
+      {/* <Footer />  */}
     </div>
   )
 }
 
-export default App;
+// function ListPage() {
 
+//   return (
+//     <div
+
+//   )
+
+// }
+
+export default App;
