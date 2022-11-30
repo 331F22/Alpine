ffa6e2b9bee9ebe15a3cb07a0aa3a36f5ff006db
NichoNaps <nicholascall6@gmail.com>
2022-11-29 23:56:11 -0700

    Added test to preview the generated image. Seems that png produces a completely black image for some reason. Png works though.

--- client/src/components/SignatureBox/SignatureBox.jsx
+++ client/src/components/SignatureBox/SignatureBox.jsx
@@ -1,10 +1,13 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import './SignatureBox.css';
 import SignatureCanvas from 'react-signature-canvas'
 
 const SignatureBox = () => {
     console.log("hi")
     let sigCanvas = useRef();
+
+    // this is here just for debugging/testing
+    const [img, setImg] = useState("");   
 
     return (
         <>
@@ -13,24 +16,32 @@ const SignatureBox = () => {
                     penColor='black' canvasProps={{ className: 'signature' }} />
             </div>
 
-            <button onClick={() => {
-                sigCanvas.clear();
-            }}>Clear</button>
+            <div className='signature-controls'>
+                <button onClick={() => {
+                    sigCanvas.clear();
+                }}>Clear</button>
 
-            <button onClick={() => {
-                // Using the non-react demo: https://jsfiddle.net/szimek/osenxvjc/
+                <button onClick={() => {
+                    // Using the non-react demo: https://jsfiddle.net/szimek/osenxvjc/
 
-                let data = sigCanvas.toData();
-                if (data) {
-                    data.pop();
-                    sigCanvas.fromData(data); // reload it after poping off the last stroke
-                }
-            }}>Undo</button>
+                    let data = sigCanvas.toData();
+                    if (data) {
+                        data.pop();
+                        sigCanvas.fromData(data); // reload it after poping off the last stroke
+                    }
+                }}>Undo</button>
 
-            <button onClick={() => {
-                console.log(sigCanvas.toDataURL("image/jpeg"))
 
-            }}>Test</button>
+
+                <button onClick={() => {
+                    console.log()
+                    
+                    setImg(sigCanvas.toDataURL("image/png"))
+
+                }}>Test</button>
+            </div>
+
+            <img src={img}></img>
         </>
     )
 }
