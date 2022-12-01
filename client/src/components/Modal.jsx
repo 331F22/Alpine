91ebaebcf98af62427699c3fe80eae27c11d341f
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2022-11-30 23:11:07 -0700

    Create the modal component and implement useModal hook

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,29 @@
+import ReactDOM from "react-dom";
+import { useContext } from "react";
+import { ModalContext } from "../context/ModalContext";
+import "./styles/Modal.css";
+
+const ModalDisplay = () => {
+  let { modalContent, handleModal, modal } = useContext(ModalContext);
+  if (modal) {
+    return ReactDOM.createPortal(
+      <div className="popup">
+          <div className="card">
+            <div className="card-body">
+              {modalContent}
+            </div>
+            <div className="card-footer text-center">
+            <button className="btn btn-danger btn-sm float-right"
+                    id="right" style={{color: 'white'}} onClick={() => handleModal()}>
+                Close
+            </button>
+            </div>
+          </div>
+      </div>,
+      // This is to create a modal-root dom element. **DON"T DELETE or the modal will not work correctly
+      document.querySelector("#modal-root")
+    );
+  } else return null;
+};
+
+export default ModalDisplay;
