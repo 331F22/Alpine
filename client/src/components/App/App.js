dd9e19ba7c6b04318d55a96541eafe17e1265542
mweide <78980098+mweide@users.noreply.github.com>
2022-12-04 21:39:53 -0700

    Update App.js

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import './App.css';
-import AddEntry from '../AddEntry.jsx';
-import CurrentEntries from '../CurrentEntries.jsx';
-import Footer from '../Footer.jsx';
-import Search from '../Search.jsx';
+import React from 'react'
+import './App.css'
+import AddEntry from '../AddEntry.jsx'
+import CurrentEntries from '../CurrentEntries.jsx'
+import Footer from '../Footer.jsx'
+import Selector from '../Selector'
 
 function App() {
 
   return (
+    <div>
     <div className="App">
-      <Search />
+      <h1 className='centered'>Welcome</h1>
+
+      <AddEntry />
+      <hr />
+      <CurrentEntries />
+      <hr />
+      <h2>Search</h2>
+      <Selector/>
+      
+      <hr />
+      <Footer />
+    </div>
     </div>
   )
 }
+
 
 export default App;
