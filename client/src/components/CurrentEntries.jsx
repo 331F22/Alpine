af89861b7a1880d91c5810788099678a176b3204
mweide <78980098+mweide@users.noreply.github.com>
2022-12-04 21:40:55 -0700

    Update CurrentEntries.jsx

--- client/src/components/CurrentEntries.jsx
+++ client/src/components/CurrentEntries.jsx
@@ -1,19 +1,28 @@
 import { useState, useRef, useEffect } from 'react';
 import axios from 'axios'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Button from '@mui/material/Button'
+import Input from '@mui/material/Input';
+
 
 const CurrentEntries = () => {
 
   const SECRET = process.env.REACT_APP_PASSCODE
 
   const [entryList, setEntryList] = useState([])
+  let [disabled, setDisabled] = useState(true)
 
-
-  // READ (GET)
+   //READ (GET)
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_HOST}/api/read`).then((response) => {
-      setEntryList(response.data)
+    setEntryList(response.data)
     })
-  }, [])
+}, [])
 
   const [newEmail, setNewEmail] = useState('')
   const [passcode, setPasscode] = useState('')
@@ -65,26 +74,30 @@ const CurrentEntries = () => {
   const refPass = useRef(null);
 
   function handleEditList(e) {
-    const collection = document.getElementsByClassName("editControls")
+    const collection = document.getElementsByClassName("editing")
     const editButton = document.getElementById('editButton')
     const doneButton = document.getElementById('doneButton')
     const editPasscodeInput = document.getElementById('editPasscodeInput')
     const submitEmailsButton = document.getElementById('submitEmailsButton')
 
     if (passcode === SECRET) {
-      for (let i = 0; i < collection.length; i++)
-        collection[i].style.display = 'block'
+      for (let i = 0; i < collection.length; i++){
+        collection[i].style.display = 'table-cell'
+      }
       doneButton.style.display = 'inline'
       editButton.style.display = 'none'
       editPasscodeInput.style.visibility = 'hidden'
       submitEmailsButton.style.display = 'block'
+      setDisabled(false)
 
     } else {
-      for (let i = 0; i < collection.length; i++)
+      for (let i = 0; i < collection.length; i++){
         collection[i].style.display = 'none'
+      }
       doneButton.style.display = 'none'
       editButton.style.display = 'inline'
       editPasscodeInput.style.visibility = 'visible'
+      setDisabled(true)
       editPasscodeInput.focus()
     }
     setPasscode('')
@@ -95,16 +108,18 @@ const CurrentEntries = () => {
     const editPasscodeInput = document.getElementById('editPasscodeInput')
     const editButton = document.getElementById('editButton')
     const doneButton = document.getElementById('doneButton')
-    const collection = document.getElementsByClassName("editControls")
+    const collection = document.getElementsByClassName("editing")
     const submitEmailsButton = document.getElementById('submitEmailsButton')
 
-    for (let i = 0; i < collection.length; i++)
+    for (let i = 0; i < collection.length; i++){
       collection[i].style.display = 'none'
+    }
     editPasscodeInput.style.visibility = 'hidden'
     doneButton.style.display = 'none'
     editButton.style.display = 'inline'
     editButton.innerHTML = "Edit List"
     submitEmailsButton.style.display = 'none'
+    setDisabled(true)
   }
 
   function checkPasscode(e) {
@@ -131,38 +146,62 @@ const CurrentEntries = () => {
   return (
 
     <div className="currentEntries posRel">
-      <h2>Current Entries</h2>
-
-      <div className='userData'>
-        {entryList.map((val, k) => {
-          return (<div key={k}>
-            <div>{val.last_name}, {val.first_name} <span className="emailListed">{val.email_address}</span> </div>
-
-            <div className="editControls editGui">
-              <button className='delete' onClick={() => {
-
-                deleteEntry(val.email_address)
-              }}>delete</button>
-              <button className='update' onClick={() => {
+<h2>Current Entries</h2>
+<TableContainer>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell align="left">First Name</TableCell>
+            <TableCell align="left">Last Name</TableCell>
+            <TableCell align="right">Email Address</TableCell>
+            <TableCell align="center" className='editing' sx={{display: 'none',}}>Edit</TableCell>
+            <TableCell align="center" className='editing' sx={{display: 'none'}}>Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {entryList.map((val, k) => (
+            <TableRow
+              key={k}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell align="left">{val.first_name}</TableCell>
+              <TableCell align="left">{val.last_name}</TableCell>
+              <TableCell align="right">
+                <Input
+                  fullWidth={true}
+                  disabled={disabled}
+                  onChange={(e) => setNewEmail(e.target.value)}
+                  className="email"
+                  defaultValue={val.email_address}
+                  /></TableCell>
+              <TableCell align="right" className='editing' sx={{display: 'none', alignContent: 'right',}}>
+              <div className='centered'>
+              <Button align="right" m={'auto'} className="update editing" sx={{display: 'none',}} variant="text" onClick={() => {
                 if (newEmail.length > 0) {
                   updateEmail(val.email_address);
                 }
-              }}>update</button>
-              <input type="email" className="updateInput" placeholder={val.email_address}
-                onChange={(e) => setNewEmail(e.target.value)} />
-            </div>
-          </div>)
+              }}>Update</Button></div></TableCell>
+              <TableCell align="right" className='editing' sx={{display: 'none', alignContent: 'right',}}>
+              <div className='centered'>
+              <Button align="right" m={'auto'} className="delete editing" sx={{display: 'none', color:"red",}} variant="text" onClick={() => {
+                deleteEntry(val.email_address)
+              }}>Delete</Button>
+              </div></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
 
-        })}
+      <div className='userData'>
         <div className="editField editGui">
-          <button id="editButton" onClick={handleEditList}>Edit List</button>
-          <button id="doneButton" onClick={handleFinishedEditing}>Finished Editing</button>
-          <input id="editPasscodeInput" ref={refPass} type="password"
+          <Button id="editButton" variant="contained" onClick={handleEditList} sx={{ textTransform: 'capitalize', borderRadius:0, color: "#b01b1f", borderColor: "#b01b1f", backgroundColor:"white", ':hover': {backgroundColor:"#b01b1f", color:"white"}, ':click': {backgroundColor:"#b01b1f", color:"white"} }}>Edit List</Button>
+          <Button id="doneButton" variant="contained" onClick={handleFinishedEditing} sx={{ textTransform: 'capitalize', borderRadius:0, color: "#b01b1f", borderColor: "#b01b1f", backgroundColor:"white", ':hover': {backgroundColor:"#b01b1f", color:"white"}, ':click': {backgroundColor:"#b01b1f", color:"white"} }}>Finished Editing</Button>
+          <br/>
+          <Input id="editPasscodeInput" ref={refPass} type="password"
             placeholder='Enter passcode' onChange={checkPasscode}
             onBlur={(e) => abortPasscodeAttempt(e.target.value)} />
         </div>
-        <button id="submitEmailsButton" className='submitBtn' onClick={() => alert('TODO: Send It!')}>Email Vouchers</button>
-
+        <Button id="submitEmailsButton" className='submitBtn' variant="contained" sx={{ textTransform: 'capitalize', borderRadius:0, color: "#b01b1f", borderColor: "#b01b1f", backgroundColor:"white", ':hover': {backgroundColor:"#b01b1f", color:"white"}, ':click': {backgroundColor:"#b01b1f", color:"white"} }} onClick={() => alert('TODO: Send It!')}>Email Vouchers</Button>
       </div>
     </div>
   )
