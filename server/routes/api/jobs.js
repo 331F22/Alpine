c0e0f57ec1ae681fa5dfd385e2aa8b9647008fde
Cole Orelup <coleorelup@gmail.com>
2022-12-05 15:47:44 -0700

    Delete working

--- server/routes/api/jobs.js
+++ server/routes/api/jobs.js
@@ -15,21 +15,23 @@ const db = mysql.createPool({ // createConnection
 // Create a job
 router.post("/", (req, res) => {
     const newJob = {
-        supervisorFirst: req.body.supervisorFirst,
-        supervisorLast: req.body.supervisorLast,
-        job: req.body.job,
-        numVolunteersReq: req.body.numVolunteersReq
+        jobName: req.query.jobName,
+        supervisorFirst: req.query.supervisorFirst,
+        supervisorLast: req.query.supervisorLast,
+        numVolunteersReq: req.query.numVolunteersReq,
     }
 
-    if (!newJob.supervisorFirst || !newJob.supervisorLast || !newJob.job || !newJob.numVolunteersReq){
+    // If any fields are empty, return 400 and error msg
+    if (!newJob.supervisorFirst || !newJob.supervisorLast || !newJob.jobName || !newJob.numVolunteersReq){
         return res.status(400).json({msg: 'Please include all fields'})
     }
 
+    // Else, add the job to the database
     else{
-        const sqlInsert = `INSERT INTO jobs (supervisorFirst, supervisorLast, job, numVolunteersReq) VALUES ("${newJob.supervisorFirst}","${newJob.supervisorLast}","${newJob.job}",${newJob.numVolunteersReq});`
+        const sqlInsert = `INSERT INTO jobs (jobName, supervisorFirst, supervisorLast, numVolunteersReq) VALUES ("${newJob.jobName}","${newJob.supervisorFirst}","${newJob.supervisorLast}",${newJob.numVolunteersReq});`
         db.query(sqlInsert, (err, result) => {
             if(err) throw err
-            res.send(req.body)
+            res.send(newJob)
         })
     }
 
@@ -43,31 +45,48 @@ router.get("/", (req, res) => {
         if(err){
             throw err;
         }
+        // Actual result data
         // res.send(result);
         res.json(result);
     })
 })
 
 // UPDATE
-router.put("/api/put", (req, res) => {
+// Update job
+router.put("/:jobID", (req, res) => {
 
-    const sqlUpdate = "UPDATE jobs SET email_address = ? WHERE email_address = ?"
+    const updateJobID = req.body;
+
+    // = {
+    //     supervisorFirst: req.body.supervisorFirst,
+    //     supervisorLast: req.body.supervisorLast,
+    //     job: req.body.job,
+    //     numVolunteersReq: req.body.numVolunteersReq
+    // }
+
+    const getJob = `SELECT * FROM jobs WHERE jobID = ${updateJobID}`
+    db.query(getJob, (err, result)=>{
+
+    })
+
+
+    const sqlUpdate = `UPDATE jobs SET ${''} = "${''}" WHERE jobID = "${updateJobID}"`
     db.query(sqlUpdate, (err, result)=>{
         if (err) throw err;
-        console.log("Database Updated: ", )
         res.send(result)
     })
 })
 
 // DELETE
-router.delete("/api/delete/:emailAddress", (req, res) => {
-    const ea = req.params.emailAddress;
-    console.log(ea)
-    const sqlDelete = "DELETE FROM volunteers WHERE email_address = ?";
-    db.query(sqlDelete, [ea], (err, result) => {
+router.delete("/", (req, res) => {
+    
+    const deleteJobID = req.query.jobID; 
+    console.log(deleteJobID)
+
+    const sqlDelete = `DELETE FROM jobs WHERE jobID = "${deleteJobID}"`;
+    db.query(sqlDelete, (err, result) => {
         if(err) throw err
-        console.log("Server: deleted: ", ea)
-        res.send(result)
+        res.send()
     })
 })
 
