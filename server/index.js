1c8e877283ce07177b1783c293d718fd17b562c0
mweide <78980098+mweide@users.noreply.github.com>
2022-12-04 21:43:28 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,24 +5,23 @@ const app = express()
 const mysql = require('mysql')
 const dotenv = require('dotenv').config()
 
-// createConnection
-const db = mysql.createPool({
+const db = mysql.createPool({ // createConnection
     host: 'localhost',
-    user: process.dotenv.DBUSER,
-    password: process.dotenv.DBPASS,
-    database: process.dotenv.DATABASE,
-    port: process.dotenv.DBPORT
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DATABASE,
+    port: process.env.DBPORT
 })
 
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-//get codes by events
-app.get("/api/read/codesByEvents/:evt", (req, res) => {
-    const evt = req.params.evt
-    const sqlSelect = "SELECT event_name, event_date, ticketCode FROM events JOIN volunteers ON events.volunteer_list_id = volunteers.volunteer_list_id JOIN tickets ON volunteers.volunteer_id = tickets.volunteer_id WHERE event_name = ?" // TODO: need query here
-    db.query(sqlSelect,[evt], (err, result) => { 
+// READ
+app.get("/api/read", (req, res) => {
+    console.log(req)
+    const sqlSelect = "SELECT * FROM volunteers;"
+    db.query(sqlSelect, (err, result) => { 
         if(err){
             throw err;
         }
@@ -30,23 +29,64 @@ app.get("/api/read/codesByEvents/:evt", (req, res) => {
     })
 })
 
-//get people by codes
+app.get("/api/read/events", (req, res) => {
+    const sqlSelect = "SELECT event_name FROM events;"
+    db.query(sqlSelect, (err, result) => { 
+        if(err){
+            throw err;
+        }
+        res.send(result);
+    })
+})
+
+app.get("/api/read/codesByEvent/", (req, res) => {
+    const sqlSelect = "SELECT event_name, event_date, ticketCode FROM events JOIN volunteer_lists ON events.volunteer_list_id=volunteer_lists.volunteer_list_id JOIN volunteers ON volunteer_lists.volunteer_id=volunteers.volunteer_id JOIN tickets ON volunteers.volunteer_id=tickets.volunteer_id WHERE event_name=?;"
+    db.query(sqlSelect, (err, result) => { //[evt], (err, result) => { 
+        if(err){
+          throw err;
+        }
+        res.send(result);
+    })
+})
+
+app.get("/api/read/codesByEvent/:evt", (req, res) => {
+    const evtt = req.params.evt
+    const sqlSelect = "SELECT event_name, event_date, ticketCode FROM events JOIN volunteer_lists ON events.volunteer_list_id=volunteer_lists.volunteer_list_id JOIN volunteers ON volunteer_lists.volunteer_id=volunteers.volunteer_id JOIN tickets ON volunteers.volunteer_id=tickets.volunteer_id WHERE event_name=?;"
+    db.query(sqlSelect, [evtt], (err, result) => { 
+        if(err){
+            throw err;
+        }
+        res.send(result);
+    })
+})
+
+app.get("/api/read/peopleByCodes/", (req, res) => {
+    //const cd = req.params.cd
+    const sqlSelect = "SELECT first_name, last_name, email_address, ticketCode FROM tickets JOIN volunteers ON tickets.volunteer_id=volunteers.volunteer_id;"
+    db.query(sqlSelect, [cd], (err, result) => { 
+        if(err){
+            throw err;
+        }
+        res.send(result);
+    })
+})
+
 app.get("/api/read/peopleByCodes/:code", (req, res) => {
-    const code = req.params.code
-    const sqlSelect = "SELECT first_name, last_name, email_address, ticketCode FROM tickets JOIN volunteers ON tickets.volunteer_id = volunteers.volunteer_id WHERE ticketCode = ?" // TODO: need query here
-    db.query(sqlSelect, [code], (err, result) => { 
+    const cd = req.params[0]
+    const sqlSelect = "SELECT first_name, last_name, email_address, ticketCode FROM tickets JOIN volunteers ON tickets.volunteer_id=volunteers.volunteer_id WHERE ticketCode=?;"
+    db.query(sqlSelect, [cd], (err, result) => { 
         if(err){
             throw err;
         }
         res.send(result);
+        console.log(cd)
     })
 })
 
-//get codes by people
-app.get("/api/read/codesByPeople", (req, res) => {
-    const first = req.body.fn
-    const last = req.body.ln
-    const sqlSelect = "SELECT ticketCode, first_name, last_name FROM volunteers JOIN tickets ON volunteers.volunteer_id = tickets.volunteer_id WHERE first_name = ? AND last_name = ?" // TODO: need query here
+app.get("/api/read/codesByPeople/:fn/:ln", (req, res) => {
+    const first = req.params.fn
+    const last = req.params.ln
+    const sqlSelect = "SELECT ticketCode, first_name, last_name FROM volunteers JOIN tickets ON volunteers.volunteer_id=tickets.volunteer_id WHERE first_name=? and last_name=?;"
     db.query(sqlSelect, [first, last], (err, result) => { 
         if(err){
             throw err;
@@ -55,11 +95,10 @@ app.get("/api/read/codesByPeople", (req, res) => {
     })
 })
 
-//get people by events
 app.get("/api/read/peopleByEvents/:evt", (req, res) => {
-    const evt = req.params.evt
-    const sqlSelect = "SELECT event_name, event_date, first_name, last_name FROM events JOIN volunteers ON events.volunteer_list_id = volunteers.volunteer_list_id WHERE event_name = ?" //TODO: need query here
-    db.query(sqlSelect, [evt], (err, result) => { 
+    const evtt = req.params.evt
+    const sqlSelect = "SELECT event_name, event_date, first_name, last_name FROM events JOIN volunteer_lists ON events.volunteer_list_id=volunteer_lists.volunteer_list_id JOIN volunteers ON volunteer_lists.volunteer_id=volunteers.volunteer_id WHERE event_name=?;"
+    db.query(sqlSelect, [evtt], (err, result) => { 
         if(err){
             throw err;
         }
@@ -67,13 +106,52 @@ app.get("/api/read/peopleByEvents/:evt", (req, res) => {
     })
 })
 
-// get list of events for event selector
-app.get("/api/read/eventsList"), (req, res) => {
-    const sqlSelect = "SELECT event_name FROM events" // TODO: need query here
-    db.query(sqlSelect, [evt], (err, result) => { 
-        if(err){
-            throw err;
-        }
-        res.send(result);
+// CREATE
+app.post("/api/create", (req, res) => {
+    const fn = req.body.first
+    const ln = req.body.last
+    const ea = req.body.email
+    const sqlInsert = "INSERT INTO volunteers (first_name, last_name, email_address) VALUES (?,?,?);"
+    db.query(sqlInsert, [fn, ln, ea], (err, result) => {
+        if(err) throw err
+        console.log("Server posted: ", fn, ln)
+        res.send(result)
     })
-}
+}) 
+
+// DELETE
+app.delete("/api/delete/:emailAddress", (req, res) => {
+    const ea = req.params.emailAddress;
+    console.log(ea)
+    const sqlDelete = "DELETE FROM volunteers WHERE email_address = ?";
+    db.query(sqlDelete, [ea], (err, result) => {
+        if(err) throw err
+        console.log("Server: deleted: ", ea)
+        res.send(result)
+    }) 
+})
+
+// UPDATE
+app.put("/api/update", (req, res) => {
+    // console.log(req)
+    
+    const ne = req.body.new;
+    const oe = req.body.old;
+    console.log("Ready to change: ", oe, "to", ne)
+    const sqlUpdate = "UPDATE volunteers SET email_address = ? WHERE email_address = ?"
+    db.query(sqlUpdate, [ne, oe], (err, result)=>{
+        if(err)  throw err;
+        console.log("Server changed: ", oe, "to", ne)
+        res.send(result)
+    })
+})
+
+const PORT = 3000 //process.env.EXPRESSPORT;
+const msg = `Running on PORT ${PORT}`
+app.get("/", (req, res) => {
+    res.send(`<h1>Express Server</h1><p>${msg}<p>`)
+})
+app.listen(PORT, () => {
+    console.log(msg)
+})
+
