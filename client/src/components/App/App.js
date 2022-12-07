09176be65b0332c1d047cd5f110f0b0c33a3df46
Jzavarelli <jlzavarelli@gmail.com>
2022-12-06 19:10:38 -0700

    update-push-jace

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import AddEntry from '../AddEntry.jsx';
+import Blacklist from '../Blacklist.jsx';
 import CurrentEntries from '../CurrentEntries.jsx';
+import Footer from '../Footer.jsx';
 
 function App() {
 
@@ -13,47 +15,12 @@ function App() {
 
       {/* <AddEntry /> */}
       {/* <hr /> */}
-      {/* <CurrentEntries /> */}
-      {/* <hr /> */}
-
-      <div id="butt">
-      <button class="btn btn-lg btn-primary active" onclick="history.back()">Click Me Boi!</button>
-      </div>
-      
-      <hr/>
-      <table id="table" class="table border table-striped table-hover">
-        
-        <thead>
-          <tr>
-            <th scope="col">First Name</th>
-            <th scope="col">Last Name</th>
-            <th scope="col">Listing</th>
-            <th scope="col">Reason</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td scope="row">first_name</td>
-            <td>last_name</td>
-            <td>white_listed</td>
-            <td>reasoning is followed</td>
-          </tr>
-          <tr>
-            <td scope="row">first_name</td>
-            <td>last_name</td>
-            <td>white_listed</td>
-            <td>reasoning is followed</td>
-          </tr>
-          <tr>
-            <td scope="row">first_name</td>
-            <td>last_name</td>
-            <td>white_listed</td>
-            <td>reasoning is followed</td>
-          </tr>
-        </tbody>
-      </table>
-
+      <Blacklist />
+      <hr />
+      <CurrentEntries />
+  
+      <hr /> 
+      <Footer />
     </div>
 
 
