3a2ca9ac45920637e827668d8ef16b8745868ac2
KA404NotFound <98042237+KA404NotFound@users.noreply.github.com>
2022-12-09 18:23:08 -0700

    fixed merging

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -20,54 +20,19 @@ function App() {
   return (
     <Router>
     <div className="App">
-<<<<<<< HEAD
       <div className="container">
 
-=======
       <Navigation />
       <Routes>
         <Route path = '/' element = {<AddEntry />} />
         <Route path = '/currententries' element = {<CurrentEntries />} />
         <Route path = '/whitelist' element = {<Whitelist />} />
       </Routes>
-<<<<<<< HEAD
-=======
->>>>>>> bdadab45ba5f26b770d8ebca97248e3f62528f6c
-      <AddEntry />
-      {/* <hr /> */}
-      {/* <CurrentEntries /> */}
-      {/* <hr /> */}
-      <br />
-<<<<<<< HEAD
->>>>>>> 854de3eda7f68a6d3a76c168abed5b31fd3fc4cb
-=======
->>>>>>> 854de3eda7f68a6d3a76c168abed5b31fd3fc4cb
 
-      {/* <div className="ListDivider">
-        <Whitelist />
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <hr />  */}
-=======
-        <br /> 
->>>>>>> 854de3eda7f68a6d3a76c168abed5b31fd3fc4cb
-=======
-        <br /> 
->>>>>>> 854de3eda7f68a6d3a76c168abed5b31fd3fc4cb
-        {/* <Footer /> */}
-<<<<<<< HEAD
-      </div>
-
-      </div>
-    </div>
-=======
-      {/* </div> */}
-
-=======
->>>>>>> 17e5d5276828efa7afeaaa8b6bde71ac399875d9
+  
      </div>
+    </div>
     </Router>
->>>>>>> bdadab45ba5f26b770d8ebca97248e3f62528f6c
   )
 }
 
