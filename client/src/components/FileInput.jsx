cee88843825725a2a9203fd16e8ef2a518e148ac
JamisonCleveland <43040811+JamisonCleveland@users.noreply.github.com>
2022-12-11 20:13:29 -0700

    Heavily refactored `FileInput`, fixed bug with not clearing file input, fixed bug with disabled submit, added an error message when an upload fails

--- client/src/components/FileInput.jsx
+++ client/src/components/FileInput.jsx
@@ -1,35 +1,43 @@
 import { useRef, useState } from 'react';
 import axios from "axios";
-import { useDropzone } from 'react-dropzone';
-
-// TODO: Drag and drop functionality for component
-// TODO: Spinner for user waiting for file to upload
-// TODO: Error messages for the user (i.e. Too many files, File too large)
 
 const FileInput = () => {
     const fileInputRef = useRef(null);
+    const fileRef = useRef(null);
     const [fileUploaded, setFileUploaded] = useState(false);
-    //const [fileUploadFailure, setFileUploadFailure] = useState(false);
-    //const {getRootProps, getInputProps} = useDropzone();
+    const [canUpload, setCanUpload] = useState(false);
+    const [uploadFail, setUploadFail] = useState(false);
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        if (!fileInputRef.current) {
-            //  TODO: add a tag that says that you can't upload with out any files selected
-            return;
-        }
+        setUploadFail(false);
+        setCanUpload(false);
         const formData = new FormData();
-        formData.append("ticket_sheet", fileInputRef.current.files[0]);
+        formData.append("ticket_sheet", fileRef.current);
         const url = `${process.env.REACT_APP_HOST}/api/ticket_sheet`;
         const config = {
             headers: {
                 'content-type': 'multipart/form-data',
             }
         };
-        setFileUploaded(true);
-        await axios.post(url, formData, config)
-        fileInputRef.current.value = null;
+        axios.post(url, formData, config).then((res) => {
+            setCanUpload(true);
+            fileInputRef.current.value = null;
+            fileRef.current = null;
+            setFileUploaded(true);
+        }).catch((err) => {
+            setCanUpload(true);
+            setUploadFail(true);
+            console.log(err);
+        })
     };
+
+    const handleFileChange = (e) => {
+        fileRef.current = e.target.files[0];
+        setCanUpload(!!fileInputRef.current);
+        setFileUploaded(false);
+        setUploadFail(false);
+    }
 
     return (
         <form encType="multipart/form-data" onSubmit={handleSubmit}>
@@ -38,9 +46,11 @@ const FileInput = () => {
                 type="file"
                 ref={fileInputRef}
                 accept=".xlsx,.csv"
+                onChange={handleFileChange}
             />
-            <input type="submit" value="Upload File(s)" disabled={fileUploaded}/>
+            <input type="submit" value="Upload File(s)" disabled={!canUpload}/>
             {fileUploaded && (<p>Uploaded!</p>)}
+            {uploadFail && (<p>Uh oh! Something on the server side didn't work.</p>)}
         </form>
     );
 };
