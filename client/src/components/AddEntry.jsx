cfb3c2dbbaf3ba918eab14e2468e5f740d5db523
ali <ali.khaef2013@gmail.com>
2022-12-12 10:29:27 -0700

    nav bar fixed logo resize fixed

--- client/src/components/AddEntry.jsx
+++ client/src/components/AddEntry.jsx
@@ -37,20 +37,20 @@ const AddEntry = () => {
   return (
     <div>
       <div className="container">
-      <div className="addEntry">
-        <h2 className="display-1">Add Volunteers</h2>
+      <div className="addEntry"style={{ width: "50%", height: "50%" }} resizeMode="cover" >
+        <h2 class="display-1">Add Volunteers</h2>
         <div id='userInput'>
           <div>
             <label htmlFor="firstName">First Name</label>
-            <input ref={ref1} id="firstName" name="firstName" type="text" className="form-control" placeholder="First Name" aria-label="First Name" aria-describedby="basic-addon1" onChange={(e) => setFirstName(e.target.value)} />
+            <input ref={ref1} id="firstName" name="firstName" type="text" class="form-control" placeholder="First Name" aria-label="First Name" aria-describedby="basic-addon1" onChange={(e) => setFirstName(e.target.value)} />
           </div>
           <div>
             <label htmlFor="lastName">Last Name</label>
-            <input ref={ref2} id="lastName" name="lastName" type="text" className="form-control" placeholder="Last Name" aria-label="Last Name" aria-describedby="basic-addon1" onChange={(e) => setLastName(e.target.value)} />
+            <input ref={ref2} id="lastName" name="lastName" type="text" class="form-control" placeholder="Last Name" aria-label="Last Name" aria-describedby="basic-addon1" onChange={(e) => setLastName(e.target.value)} />
           </div><br />
           <div className="emailField" >
             <label htmlFor="email">Email Address</label>
-            <input ref={ref3} id="email" type="email" className="form-control" placeholder="Email@example.com" aria-label="Email" aria-describedby="basic-addon1"  name="email" onChange={(e) => setEmailAddress(e.target.value)} />
+            <input ref={ref3} id="email" type="email" class="form-control" placeholder="Email@example.com" aria-label="Email" aria-describedby="basic-addon1"  name="email" onChange={(e) => setEmailAddress(e.target.value)} />
           </div>
           <button type="button" className="btn btn-outline-dark submitBtn"
             onClick={() => {
@@ -64,7 +64,12 @@ const AddEntry = () => {
       </div>
       </div>
       <img
+            style={{  position: 'absolute',
+            right: 5,
+            top: 5,
+            bottom: 0, width: "15%", height: "25%" }}
             src="https://uploads-ssl.webflow.com/57b4d56c1f986d4879b0574d/58c72f55fd3103f64ceef729_alpine%20logo%20transp-p-500x417.png"
+            resizeMode="cover"
             alt="logo"></img>
       
     </div>
