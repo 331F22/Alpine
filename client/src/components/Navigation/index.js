6408110f16637d47a3a63eea18a32712b7fe62a1
ali <ali.khaef2013@gmail.com>
2022-12-12 10:38:06 -0700

    nav fixed

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -7,14 +7,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Navigation = () => {
     return (
         <div id="nav">
-            <ul class="list-group">
-            <li class="list-group-item"><Link to='/' activeStyle>
+            <ul>
+            <li class="bg-danger"><Link to='/' activeStyle>
                 Home
             </Link></li>
-            <li class="list-group-item"><Link to='/currententries' activeStyle>
+            <li class="bg-danger"><Link to='/currententries' activeStyle>
                 Current Entries
             </Link></li>
-            <li class="list-group-item"><Link to='/whitelist' activeStyle>
+            <li class="bg-danger"><Link to='/whitelist' activeStyle>
                 Whitelist
             </Link></li>
             </ul>
