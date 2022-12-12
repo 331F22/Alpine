c5fa89003c7b108ea20a0bde73026c90ab08ae93
mweide <78980098+mweide@users.noreply.github.com>
2022-12-12 14:36:21 -0700

    Update SetInputs.jsx

--- client/src/components/SetInputs.jsx
+++ client/src/components/SetInputs.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import EventSelector from "./EventSelector";
 import PeopleInput from "./PeopleInput";
 import CodeInput from "./CodeInput";
+import Button from '@mui/material/Button';
 
 const SetInputs = (props) => {
     const searchType = props.searchType
@@ -19,7 +20,11 @@ const SetInputs = (props) => {
     if(searchType === "peopleByEvents"){
         return (<div><br/><EventSelector searchType={searchType}/></div>)
     }
-    
+        if(searchType === "emailList") {
+        return (<div><br/><Button id="showEmails" className='showEmails' variant="contained" sx={{ textTransform: 'capitalize', borderRadius:0, color: "#b01b1f", borderColor: "#b01b1f", backgroundColor:"white", ':hover': {backgroundColor:"#b01b1f", color:"white"}, ':click': {backgroundColor:"#b01b1f", color:"white"} }}>Search</Button></div>)}
+        if(searchType === "issuedTickets"){
+                return (<div><br/><Button id="showTickets" className='showTickets' variant="contained" sx={{ textTransform: 'capitalize', borderRadius:0, color: "#b01b1f", borderColor: "#b01b1f", backgroundColor:"white", ':hover': {backgroundColor:"#b01b1f", color:"white"}, ':click': {backgroundColor:"#b01b1f", color:"white"} }}>Search</Button></div>)}
+
 }
 
 export default SetInputs
